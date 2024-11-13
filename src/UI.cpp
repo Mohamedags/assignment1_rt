00f678b0507bfc6709bd370fca90f82a3f64f6a1
Mohamedags <aguenarousm26@gmail.com>
2024-11-13 14:02:55 +0100

    Spawn a new turtle in the environment: turtle2

--- src/UI.cpp
+++ src/UI.cpp
@@ -2,13 +2,41 @@
 #include "turtlesim/Pose.h"
 #include "geometry_msgs/Twist.h"
 #include "turtlesim/Spawn.h"
-#include "turtlesim/Kill.h"
 
 
 
+// Declare the ROS Publisher for controlling turtle's movement
+ros::Publisher pub;
+
+void positionCallback(const turtlesim::Pose::ConstPtr& msg) {
+
+
+}
 
 int main(int argc, char** argv) {
-    
 
+    //Initialize the ROS node
+    ros::init(argc,argv, "turtlebot_subscriber");
+    ros::NodeHandle nh;
+    
+    //Subscriber to the turtle's pose
+    ros::Subscriber sub = nh.subscribe("turtle1/pose", 1000,positionCallback);
+
+    //Create the service clients
+    ros::ServiceClient client1 = nh.serviceClient<turtlesim::Spawn>("/spawn");
+    
+    
+        // Spawn a new turtle (turtle2)
+    turtlesim::Spawn srv1;
+    srv1.request.x = 2.0;
+    srv1.request.y = 1.0;
+    srv1.request.theta = 0.0;
+    srv1.request.name = "turtle2";
+    client1.call(srv1);
+     
+    
+    
+    //ros::spinOnce();
+    
     return 0;
 }
