f0ce5d5f88f7b11035466398ba69936dd4155997
Mohamedags <aguenarousm26@gmail.com>
2024-11-23 15:13:08 +0100

    implementing a simple textual interface to get user data in cpp, the user can choose which turtle to control and its linear and angular velocity making sure the command is running for 1s then asks the user again to enter data

--- src/UI.cpp
+++ src/UI.cpp
@@ -1,6 +1,19 @@
 #include "ros/ros.h"
 #include "turtlesim/Spawn.h"
+#include <iostream>
+#include "geometry_msgs/Twist.h"
 
+ros::Publisher pub;
+
+void getUserInput(std::string &robot, double &linear_velocity, double &angular_velocity) {
+    std::cout << "Available robots: turtle1, turtle2" << std::endl;
+    std::cout << "Enter the robot you want to control (turtle1/turtle2): ";
+    std::cin >> robot;
+    std::cout << "Enter the linear velocity for " << robot << " (number): ";
+    std::cin >> linear_velocity;
+    std::cout << "Enter the angular velocity for " << robot << " (number): ";
+    std::cin >> angular_velocity;
+}
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "turtlebot_control");
@@ -14,8 +27,40 @@ int main(int argc, char** argv) {
     srv1.request.theta = 0.0;
     srv1.request.name = "turtle2";
     client1.call(srv1);
+
+    std::string robot;
+    double linear_velocity, angular_velocity;
+    geometry_msgs::Twist cmd_vel;
+    
+    ros::Rate loop_rate(10);
+    
+    pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+
+    while (ros::ok()) {
+        getUserInput(robot, linear_velocity, angular_velocity);
         
-    ros::spin();
+        
+        if (robot == "turtle1" && pub.getTopic() != "/turtle1/cmd_vel") {
+            pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+        } else if (robot == "turtle2" && pub.getTopic() != "/turtle2/cmd_vel") {
+            pub = nh.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 10);
+        }
+        
+        cmd_vel.linear.x = linear_velocity;
+        cmd_vel.angular.z = angular_velocity;
+
+        ros::Time start_time = ros::Time::now();
+       
+        while (ros::ok() && (ros::Time::now() - start_time).toSec() <= 1) {
+            pub.publish(cmd_vel);
+            loop_rate.sleep();
+        }
+
+        cmd_vel.linear.x = 0;
+        cmd_vel.angular.z = 0;
+        pub.publish(cmd_vel);
+        loop_rate.sleep();
+    }
 
     return 0;
 }
