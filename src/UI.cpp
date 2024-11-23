09df54c7a2a36aa3d8285fe9f60bf7cc7e3fe96b
Mohamedags <aguenarousm26@gmail.com>
2024-11-23 12:46:05 +0100

    spawning new turtle2 in UI_node using cpp

--- src/UI.cpp
+++ src/UI.cpp
@@ -1,6 +1,21 @@
 #include "ros/ros.h"
+#include "turtlesim/Spawn.h"
+
 
 int main(int argc, char** argv) {
+    ros::init(argc, argv, "turtlebot_control");
+    ros::NodeHandle nh;
     
+    ros::ServiceClient client1 = nh.serviceClient<turtlesim::Spawn>("/spawn");
+    
+    turtlesim::Spawn srv1;
+    srv1.request.x = 2.0;
+    srv1.request.y = 1.0;
+    srv1.request.theta = 0.0;
+    srv1.request.name = "turtle2";
+    client1.call(srv1);
+        
+    ros::spin();
+
     return 0;
 }
