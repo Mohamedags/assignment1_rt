e5d2b45ac19cdd6243c994d193af6af8c0206920
Mohamedags <aguenarousm26@gmail.com>
2024-11-24 00:15:46 +0100

    Node 1: spawning new turtle using python

--- scripts/UI.py
+++ scripts/UI.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 import rospy
-
+from turtlesim.srv import Spawn
 
 def UI():
-        pass
+
+    rospy.init_node('turtle_control_ui', anonymous=True)
+    rospy.wait_for_service('/spawn')
+    try:
+        spawn_turtle = rospy.ServiceProxy('/spawn', Spawn)
+        
+        spawn_turtle(2.0, 1.0, 0.0, 'turtle2')
+        rospy.loginfo("Turtle 2 was spawned successfully!")
+    except rospy.ServiceException as e:
+        rospy.logerr("Service was failed: %s", e)
         
 
 if __name__ == '__main__':
