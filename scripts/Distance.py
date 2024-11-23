433ac58bec15f2fa0540773283f59f7c4d5409a7
Mohamedags <aguenarousm26@gmail.com>
2024-11-23 23:59:17 +0100

    Node 1: Initial commit -empty codes- using python

---
+++ scripts/Distance.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import rospy
+
+
+def Distance():
+        pass  
+    
+        
+if __name__ == '__main__':
+    try:
+        Distance()
+    except rospy.ROSInterruptException:
+        pass
---
+++ scripts/UI.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import rospy
+
+
+def UI():
+        pass
+        
+
+if __name__ == '__main__':
+    try:
+        UI()
+    except rospy.ROSInterruptException:
+        pass
+
